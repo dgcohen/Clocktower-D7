b9a224ccf0cfd71dbb0a0498527bea5c9a26e71f
Diego Cohen <diego.cohen3@gmail.com>
2015-09-28 15:09:59 -0400

    Added blue monday theme and assets to clocktower player module directory

--- sites/all/modules/clocktower_player/js/clocktower-player.js
+++ sites/all/modules/clocktower_player/js/clocktower-player.js
@@ -4,23 +4,23 @@
     $("#header").hide();
     $("#page-title").hide();
 
-    $("#jquery_jplayer_1").jPlayer({
-            ready: function () {
-              $(this).jPlayer("setMedia", {
-                title: "Bubble",
-                m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-                oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-              });
-            },
-            cssSelectorAncestor: "#jp_container_1",
-            swfPath: "/js",
-            supplied: "m4a, oga",
-            useStateClassSkin: true,
-            autoBlur: false,
-            smoothPlayBar: true,
-            keyEnabled: true,
-            remainingDuration: true,
-            toggleDuration: true
-          });
+  	$("#jquery_jplayer_1").jPlayer({
+  		ready: function (event) {
+  			$(this).jPlayer("setMedia", {
+  				title: "Bubble",
+  				m4a: "http://jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+  				oga: "http://jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+  			});
+  		},
+  		swfPath: "../../../libraries/jplayer",
+  		supplied: "m4a, oga",
+  		wmode: "window",
+  		useStateClassSkin: true,
+  		autoBlur: false,
+  		smoothPlayBar: true,
+  		keyEnabled: true,
+  		remainingDuration: true,
+  		toggleDuration: true
+  	});
   });
 })(jQuery);
