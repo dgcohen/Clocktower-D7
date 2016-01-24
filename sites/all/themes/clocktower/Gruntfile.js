122a3cf3b745f55c1f301b87309dba1eead02eb5
Diego Cohen <diego.cohen3@gmail.com>
2016-01-24 17:05:49 -0500

    Configured grunt and added necessary modules

---
+++ sites/all/themes/clocktower/Gruntfile.js
@@ -0,0 +1,303 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  var PHP_FILES = [
+    '*.info',
+    '{,**}/*.{php,inc}'
+  ];
+  var IMAGE_FILES = [
+    'images/**'
+  ];
+  var IMAGE_FILES_ICONS = [
+    'images/icons2x/*.png'
+  ];
+  var CSS_FILES = [
+    'css/{,**/}*.css'
+  ];
+  var SASS_FILES = [
+    'sass/{,**/}*.{scss,sass}'
+  ];
+  var JS_FILES_NO_MIN = [
+    'js/{,**/}*.js',
+    '!js/{,**/}*.min.js'
+  ];
+
+  // https://github.com/Ensighten/grunt-spritesmith/issues/27
+  // allows for cache busting of sprite images
+  var md5 = require('md5');
+  var fs = require('fs');
+
+  function getCollectiveMd5(files) {
+    var expandedFiles = grunt.file.expand(files),
+        collectiveContent = expandedFiles.reduce(function (content, file) {
+          return content + fs.readFileSync(file, 'binary');
+        }, '');
+    return md5(collectiveContent);
+  }
+
+  var spritecachebust = getCollectiveMd5(IMAGE_FILES_ICONS);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower: grunt.file.readJSON('bower.json'),
+    bower_config: grunt.file.readJSON('.bowerrc'),
+
+    watch: {
+      options: { },
+      sass: {
+        files: SASS_FILES,
+        tasks: ['css:dev']
+      },
+      registry: {
+        files: PHP_FILES,
+        tasks: ['shell']
+      },
+      images: {
+        files: IMAGE_FILES
+      },
+      css: {
+        files: CSS_FILES
+      },
+      js: {
+        files: JS_FILES_NO_MIN,
+        // NOTE: jshint comes before general JS tasks, so it could abort before
+        // general JS tasks are complete.
+        tasks: ['jshint', 'js:dev']
+      }
+    },
+
+    shell: {
+      all: {
+        command: 'drush cache-clear theme-registry'
+      }
+    },
+
+    // FIXME: this could be used at some point later to dynamically resize the
+    // 2x icons down to 1x
+    image_resize: {
+      options: {
+        width: '50%',
+        overwrite: true
+      },
+      all: {
+        src : IMAGE_FILES_ICONS,
+        dest : 'images/icons/'
+      }
+    },
+
+    // FIXME: this could be used at some point later to combine the retina icons
+    // into a single folder with the normal icons to allow spritesmith to
+    // generate retina sprites in a more conventional way.
+    copy: {
+      all: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'images/icons2x/',
+          dest: 'images/icons/',
+          src: ['*.png'],
+          // rename image.png in src folder to to image-2x.png in dest folder.
+          rename: function(dest, src) {
+            return dest + src.replace(/\.(\w+)$/,'-2x.$1');
+          }
+        }]
+      }
+    },
+
+    sprite:{
+      all: {
+        src             : IMAGE_FILES_ICONS,
+        dest            : 'images/generated/sprite-' + spritecachebust + '.png',
+
+        // FIXME: retina sprite generate is commented out. we have problems with a
+        // few images re: retinaSrcFilter sorting and image size.
+        //
+        // src             : 'images/icons/*.png',
+        // retinaSrcFilter : 'images/icons/*-2x.png',
+        // dest            : 'images/generated/sprite.png',
+        // retinaDest      : 'images/generated/sprite-2x.png',
+
+        destCss         : 'sass/variables/_spritemap.scss',
+        cssFormat       : 'scss_maps',
+
+        // FIXME: need imgPath set explicitly because relative paths resolve
+        // one level up from miss the 'mailman' folder. odd. need to figure out
+        // why.
+        imgPath         : '/sites/all/themes/custom/clocktower/images/generated/sprite-' + spritecachebust + '.png',
+        // retinaImgPath   : '/sites/all/themes/custom/mailman/images/generated/sprite-2x.png',
+
+        // same as from compass config
+        algorithm       : "left-right",
+        padding         : 20
+      }
+    },
+
+    sass_globbing: {
+      options: {
+        useSingleQuotes: false
+      },
+      all: {
+        files: {
+          'sass/variables.scss'    : 'sass/variables/**/*.scss',
+          'sass/abstractions.scss' : 'sass/abstractions/**/*.scss',
+          'sass/overrides.scss'    : 'sass/overrides/**/*.scss',
+          'sass/base.scss'         : 'sass/base/**/*.scss',
+          'sass/components.scss'   : 'sass/components/**/*.scss'
+        }
+      }
+    },
+
+    sass: {
+      // see https://github.com/sass/node-sass#options
+      options: {
+        sourceMap: true,
+        outputStyle : 'nested', // nested, expanded, compact, compressed
+        sourceComments : true,
+        // bower packages @import'ed in our CSS
+        // FIXME: avoid hardcoding all of this. (thought: IIFE + somehow
+        // leverage bower.json)
+        includePaths : [
+          "<%= bower_config.directory %>/normalize-scss",
+          "<%= bower_config.directory %>/compass-mixins/lib",
+          "<%= bower_config.directory %>/breakpoint-sass/stylesheets",
+          "<%= bower_config.directory %>/susy/sass"
+        ]
+      },
+      dev : {
+        options : { },
+        files: {
+          'css/clocktower.hacks.css'    : 'sass/clocktower.hacks.scss',
+          'css/clocktower.no-query.css' : 'sass/clocktower.no-query.scss',
+          'css/clocktower.normalize.css': 'sass/clocktower.normalize.scss',
+          'css/clocktower.styles.css'   : 'sass/clocktower.styles.scss'
+        }
+      },
+      dist : {
+        options : {
+          outputStyle : 'compressed',
+          sourceComments : false
+        },
+        files: "<%= sass.dev.files %>"
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: JS_FILES_NO_MIN
+    },
+
+    uglify: {
+      dev: {
+        options: {
+          mangle: false,
+          compress: false,
+          beautify: true
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          dest: 'js',
+          src: JS_FILES_NO_MIN,
+          rename: function(dest, src) {
+            var folder = src.substring(0, src.lastIndexOf('/'));
+            var filename = src.substring(src.lastIndexOf('/'), src.length);
+            filename = filename.substring(0, filename.lastIndexOf('.'));
+            return dest + '/' + folder + filename + '.min.js';
+          }
+        }]
+      },
+      dist: {
+        options: {
+          mangle: true,
+          compress: true
+        },
+        files: "<%= uglify.dev.files %>"
+      }
+    },
+
+    notify_hooks: {
+      options: {
+        enabled: true,
+        success: true
+      }
+    }
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-sass-globbing');
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.loadNpmTasks('grunt-image-resize');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-notify');
+
+  grunt.task.run('notify_hooks');
+
+  grunt.registerTask('css', 'Run all tasks related to CSS compilation.', function(target) {
+    // expect target passed. if no target, pull it from GRUNT_TARGET, otherwise
+    // default to 'dev'
+    if (target === undefined) {
+      target = process.env['GRUNT_TARGET'] || 'dev';
+      console.log("No target passed. Defaulting to '" + target + "'.");
+    }
+
+    grunt.task.run(
+      // FIXME: add these tasks back in once we have retina sprite generation
+      // worked out
+      // 'image_resize',
+      // 'copy',
+      'sprite',
+      'sass_globbing',
+      'sass:' + target
+    );
+  });
+
+  grunt.registerTask('js', 'Run all tasks related to JS (linting, minification).', function(target) {
+    // expect target passed. if no target, pull it from GRUNT_TARGET, otherwise
+    // default to 'dev'
+    if (target === undefined) {
+      target = process.env['GRUNT_TARGET'] || 'dev';
+      console.log("No target passed. Defaulting to '" + target + "'.");
+    }
+
+    if (target === "dev") {
+      // run jshint after uglify so the task does not abort due to linting
+      // issues. it should really abort, but let's not change horses midstream.
+      //
+      // FIXME: disabled jshint because we don't seem to pay attention to
+      // jshint's output, and should we switch to 'dev' on CI for any reason,
+      // the grunt task should not abort.
+      grunt.task.run(
+        'uglify:' + target
+        // 'jshint'
+      );
+    } else {
+      grunt.task.run(
+        'uglify:' + target
+      );
+    }
+  });
+
+  grunt.registerTask('build', 'Run all front-end related build tasks.', function(target) {
+    // expect target passed. if no target, pull it from GRUNT_TARGET, otherwise
+    // default to 'dev'
+    if (target === undefined) {
+      target = process.env['GRUNT_TARGET'] || 'dev';
+      console.log("No target passed. Defaulting to '" + target + "'.");
+    }
+
+    grunt.task.run(
+      'css:' + target,
+      'js:' + target
+    );
+  });
+
+};
--- sites/all/themes/clocktower/config.rb
+++ sites/all/themes/clocktower/config.rb
@@ -1,56 +1,45 @@
-#
-# This file is only needed for Compass/Sass integration. If you are not using
-# Compass, you may safely ignore or delete this file.
-#
-# If you'd like to learn more about Sass and Compass, see the sass/README.txt
-# file for more information.
-#
+##
+## This file is only needed for Compass/Sass integration. If you are not using
+## Compass, you may safely ignore or delete this file.
+##
+## If you'd like to learn more about Sass and Compass, see the sass/README.txt
+## file for more information.
+##
 
-
-# Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
-#environment = :production
-
-# In development, we can turn on the FireSass-compatible debug_info.
-firesass = false
-#firesass = true
-
+# Default to development if environment is not set.
+saved = environment
+if (environment.nil?)
+  environment = :development
+else
+  environment = saved
+end
 
 # Location of the theme's resources.
-css_dir         = "css"
-sass_dir        = "sass"
-extensions_dir  = "sass-extensions"
-images_dir      = "images"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+generated_images_dir = images_dir + "/generated"
 javascripts_dir = "js"
 
-
 # Require any additional compass plugins installed on your system.
-#require 'ninesixty'
-#require 'zen-grids'
-
-# Assuming this theme is in sites/*/themes/THEMENAME, you can add the partials
-# included with a module by uncommenting and modifying one of the lines below:
-#add_import_path "../../../default/modules/FOO"
-#add_import_path "../../../all/modules/FOO"
-#add_import_path "../../../../modules/FOO"
-
+require 'rgbapng'
+require 'toolkit'
+require 'breakpoint'
+require 'susy'
+require 'sass-globbing'
 
 ##
 ## You probably don't need to edit anything below this.
 ##
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+# You can select your preferred output style here (:expanded, :nested, :compact
+# or :compressed).
+output_style = (environment == :production) ? :expanded : :nested
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server root.
+# the absolute path to the theme from the server omega.
 relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+# Output source maps in development mode.
+sass_options = (environment == :production) ? {} : {:sourcemap => true}
