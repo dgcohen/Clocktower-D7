c08c5c2c7c2f24870b52165e77acf2f7202b0dfc
Diego Cohen <diego.cohen3@gmail.com>
2016-01-27 23:05:26 -0500

    Added template for homepage links

--- sites/all/themes/clocktower/templates/blocks/bean--homepage-links.tpl.php
+++ sites/all/themes/clocktower/templates/blocks/bean--homepage-links.tpl.php
@@ -28,10 +28,32 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
   <div class="content"<?php print $content_attributes; ?>>
     <?php
-      print render($content);
+      foreach ($content['field_homepage_links']['#items'] as $item) {
+        $ids[] = $item['value'];
+      }
+      $links = field_collection_item_load_multiple($ids);
+      foreach ($links as $delta => $link) {
+        $title = field_get_items('field_collection_item', $link,'field_link_title')[0]['value'];
+        $url = field_get_items('field_collection_item', $link, 'field_url')[0]['value'];
+        $desc = field_get_items('field_collection_item', $link, 'field_short_description')[0]['value'];
+        $image_display = field_info_instance('field_collection_item', 'field_image', $link->field_name)['display'];
+        $image = field_view_field('field_collection_item', $link, 'field_image', $image_display['default']);
+        $image_style = $image[0]['#image_style'];
+        $image_path = $image[0]['#item']['uri'];
+        $image_url = image_style_url($image_style, $image_path);
+        print theme('html_tag', array(
+          'element' => array(
+          '#tag' => 'a',
+          '#attributes' => array(
+            'href' => $url,
+            'style' => 'background-image: url(' . $image_url . ')',
+            ),
+          '#value' => '<div class="title">' . $title . '</div><div class="description">' . $desc . '</div>',
+          ),
+        ));
+      }
     ?>
   </div>
 </div>
