9fd925d986bdfff4d4359d92a3bbfe4752d9aa8c
Diego Cohen <diego.cohen3@gmail.com>
2016-03-11 10:31:17 -0500

    Completed radio index page

--- sites/all/themes/clocktower/js/clocktower.behaviors.js
+++ sites/all/themes/clocktower/js/clocktower.behaviors.js
@@ -1,40 +1,5 @@
 (function ($) {
 
-  /**
-   * The recommended way for producing HTML markup through JavaScript is to write
-   * theming functions. These are similiar to the theming functions that you might
-   * know from 'phptemplate' (the default PHP templating engine used by most
-   * Drupal themes including Omega). JavaScript theme functions accept arguments
-   * and can be overriden by sub-themes.
-   *
-   * In most cases, there is no good reason to NOT wrap your markup producing
-   * JavaScript in a theme function.
-   */
-
-  /**
-   * Behaviors are Drupal's way of applying JavaScript to a page. In short, the
-   * advantage of Behaviors over a simple 'document.ready()' lies in how it
-   * interacts with content loaded through Ajax. Opposed to the
-   * 'document.ready()' event which is only fired once when the page is
-   * initially loaded, behaviors get re-executed whenever something is added to
-   * the page through Ajax.
-   *
-   * You can attach as many behaviors as you wish. In fact, instead of overloading
-   * a single behavior with multiple, completely unrelated tasks you should create
-   * a separate behavior for every separate task.
-   *
-   * In most cases, there is no good reason to NOT wrap your JavaScript code in a
-   * behavior.
-   *
-   * @param context
-   *   The context for which the behavior is being executed. This is either the
-   *   full page or a piece of HTML that was just added through Ajax.
-   * @param settings
-   *   An array of settings (added through drupal_add_js()). Instead of accessing
-   *   Drupal.settings directly you should use this because of potential
-   *   modifications made by the Ajax callback that also produced 'context'.
-   */
-
   Drupal.behaviors.addIE10 = {
     attach: function (context, settings) {
 
@@ -64,5 +29,35 @@
       });
     }
   };
+
+  Drupal.behaviors.radioIndexLists = {
+    attach: function (context, settings) {
+
+      if($('.block-bean-radio-index')) {
+        var $seriesList = $('.view-series-list .view-content');
+        var $hostsList = $('.view-hosts-list .view-content');
+
+        splitIntoColumns($seriesList);
+        splitIntoColumns($hostsList);
+
+        function splitIntoColumns(container) {
+          var $items = container.find('.views-row');
+          container.prepend('<div class="col-left"></div><div class="col-right"></div>');
+          var $colLeft = container.find('.col-left');
+          var $colRight = container.find('.col-right');
+          var half = $items.length / 2;
+          $items.each(function(index) {
+            if (index < half) {
+              $(this).appendTo($colLeft);
+            } else {
+              $(this).appendTo($colRight);
+            }
+          });
+
+        }
+      }
+    }
+  };
+
 
 })(jQuery);
--- sites/all/themes/clocktower/templates/blocks/bean--radio-index.tpl.php
+++ sites/all/themes/clocktower/templates/blocks/bean--radio-index.tpl.php
@@ -1,14 +1,18 @@
 <div class="radio-index-header">
-  <h2>Radio</h2>
+  <h2 class="label">Radio</h2>
   <p>Music ranging from live recordings of the widely acclaimed summer Warm Up series to 
   experimental music surveys; and partnered with numerous arts organizations to programs, events, and performances.</p>
 </div>
 <div class="channel-index">
   <?php print views_embed_view('channels', 'default'); ?>
 </div>
-<div class="series-list">
-  <?php print views_embed_view('series_list', 'default'); ?>
-</div>
-<div class="hosts-list">
-  <?php print views_embed_view('hosts_list', 'default'); ?>
+<div class="series-hosts-container">
+  <div class="series-list">
+    <h2 class="label">Series</h2>
+    <?php print views_embed_view('series_list', 'default'); ?>
+  </div>
+  <div class="hosts-list">
+    <h2 class="label">Hosts</h2>
+    <?php print views_embed_view('hosts_list', 'default'); ?>
+  </div>
 </div>
